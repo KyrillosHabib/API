286e13a9f8b1c5806ed54679ab67239110468891
kyrillosaziz <kyrillos.aziz@webenia.com>
2025-07-24 10:45:25 +0300

    update program

--- API-CICD/Program.cs
+++ API-CICD/Program.cs
@@ -8,13 +8,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction()) // أضف Production
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // لو مش عامل شهادة SSL
 app.UseAuthorization();
 app.MapControllers();
-app.Run();
+
+// ⭐ شغّل التطبيق على البورت اللي Railway بيوفره
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
